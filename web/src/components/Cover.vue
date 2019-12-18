fdff6904d21cf2d52b2f21e0b1222613f69af129
Benno Lin <blueworrybear@gmail.com>
2019-12-18 23:54:45 +0800

    init

---
+++ web/src/components/Cover.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1 class="title" ref="title">Bear</h1>
+    <div style="position: relative;">
+      <parallax :fixed="true">
+        <img src="@/assets/cover.jpg">
+      </parallax>
+    </div>
+  </div>
+</template>
+
+<script>
+import Parallax from 'vue-parallaxy'
+
+export default {
+  name: 'Cover',
+  components: {
+    Parallax
+  },
+  mounted: function () {
+    window.addEventListener('resize', this.windowResize)
+  },
+  methods: {
+    windowResize (e) {
+      console.log('resize')
+      console.log(this.$refs.title.clientHeight)
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+  .title {
+    // position: absolute;
+    // left: 50px;
+    // top: 0px;
+    vertical-align: middle;
+  }
+</style>
---
+++ web/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <Cover/>
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Cover from '@/components/Cover.vue'
+import HelloWorld from '../components/HelloWorld.vue'
+export default {
+  name: 'home',
+  components: {
+    Cover,
+    HelloWorld
+  }
+}
+</script>
